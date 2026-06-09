7a3bf958c48472cdee9c30417f092ff8d740101b
Mihail Stefanov <mihail.n.stefanov@gmail.com>
2026-06-09 17:03:31 +0300

    created Click model

--- backend/apps/analytics/models.py
+++ backend/apps/analytics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from apps.links.models import Link
 
-# Create your models here.
+class Click(models.Model):
+    link = models.ForeignKey(Link, on_delete = models.CASCADE, related_name = "clicks")
+    clicked_at = models.DateTimeField(auto_now_add = True, db_index = True)
+    referrer = models.CharField(max_length = 500, blank = True, null = True)
+    user_agent = models.CharField(max_length = 500, blank = True, null = True)
+    ip_hash = models.CharField(max_length = 64)
+    country_code = models.CharField(max_length = 2, blank = True, null = True)
